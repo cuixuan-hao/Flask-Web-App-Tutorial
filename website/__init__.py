d01e518af6bf529e868474a1025d35ffbd36267a
cuixuan-hao <xuanhaocui@163.com>
2025-07-22 11:41:29 +0800

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
@@ -19,7 +20,7 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
 
     from .models import User, Note
-
+    
     with app.app_context():
         db.create_all()
 
@@ -33,7 +34,8 @@ def create_app():
 
     return app
 
+
 def create_database(app):
-    if not path.exists(DB_NAME):
+    if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
-        print(f'Created database: {DB_NAME}')
+        print('Created Database!')
